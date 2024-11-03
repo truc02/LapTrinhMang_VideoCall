925137bbd3de2a473e3ea41646688b81f867e373
khoaanhbui24 <buianhkhoa23022004@gmail.com>
2024-11-03 13:28:07 +0700

    khoa login logout

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignIn } from '@clerk/nextjs';
 
+// Đăng nhập
 export default function SiginInPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs';
-
+// Đăng ký
 export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
